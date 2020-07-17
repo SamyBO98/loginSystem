47c6dd4d68bc1579f9c0123b0d32cd21f332b046
BEN OTHMAN SAMY p1707136 <samy.ben-othman@etu.univ-lyon1.fr>
2020-07-17 17:11:59 +0200

    Ajout d'un formulaire pour se log

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <link href="style.css" rel="stylesheet">
     <head>
         <meta charset="utf-8">
-        <meta name="description" content="Login">
+        <meta name="description" content="LoginSystem">
         <title></title>
     </head>
     <body>
@@ -13,6 +13,17 @@
             <a href="#"> 
                 <img src="img/img.png" alt="logo" class="BeginningImage">            
             </a>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="#">About the Website</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+            <div>
+                <form action="includes/login.inc.php" method="post" class="Sign">
+                <input type="text" name="mailuid" placeholder="E-mail">
+                <input type="password" name="pwd" placeholder="Password">
+                </form>
+            </div>
         </nav>
     </header>
     </body>
