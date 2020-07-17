c7c643b6902287cb5ccaaa74ed26554f6b7e383d
BEN OTHMAN SAMY p1707136 <samy.ben-othman@etu.univ-lyon1.fr>
2020-07-17 17:02:44 +0200

    Initial commit

---
+++ header.php
@@ -0,0 +1,19 @@
+<html>
+<link href="style.css" rel="stylesheet">
+    <head>
+        <meta charset="utf-8">
+        <meta name="description" content="Login">
+        <title></title>
+    </head>
+    <body>
+
+
+    <header>
+        <nav>
+            <a href="#"> 
+                <img src="img/img.png" alt="logo" class="BeginningImage">            
+            </a>
+        </nav>
+    </header>
+    </body>
+</html>
