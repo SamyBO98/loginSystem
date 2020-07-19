2b1502911c6ce25fa06725a41e553b9e52472d21
BEN OTHMAN SAMY p1707136 <samy.ben-othman@etu.univ-lyon1.fr>
2020-07-19 19:53:05 +0200

    Ajout de messages d'erreur en javascript

--- signup.php
+++ signup.php
@@ -5,6 +5,23 @@
         <img src="img/logo.png" class="logo">
     </a>
         <h1>Sign Up</h1>
+        <?php
+            if(isset($_GET['error'])){
+                if($_GET['error'] == "emptyfields"){
+                    echo "<script>alert(\"Veuillez remplir tout les champs \")</script>";
+                }
+                else if($_GET['error'] == "invalidmailuid"){
+                    echo "<script>alert(\"Mail invalide \")</script>";
+                }
+                // Refaire des else if pour tout les messages d'erreurs
+            }
+            if(isset($_GET['signup'])){
+            if($_GET['signup'] == "success"){
+                    echo "<script>alert(\"Enregistrer \")</script>";
+                }
+            }
+            
+        ?>
             <div class="login-page">
                 <div class="form">
                     <form action="includes/signup.inc.php" method="post" class="login-form">
