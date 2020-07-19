0117b40509f4e1b023cf107da3bc60ca7b22d646
BEN OTHMAN SAMY p1707136 <samy.ben-othman@etu.univ-lyon1.fr>
2020-07-19 21:11:06 +0200

    aaa

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
       <?php
       if(isset($_SESSION['userId'])){
         echo '<p1>You are logged in!</p1>';
+        // mettre le html du site ici
       }
       else{
         echo  '<p2>You are logged out!</p2>';
